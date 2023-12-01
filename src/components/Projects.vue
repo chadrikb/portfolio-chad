f6aab9e2072181833dfab08291370ae29435f7b6
Chadrik Bertrand <2037079@cstj.qc.ca>
2023-12-01 15:30:25 -0500

    jpg

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -59,7 +59,7 @@
         <!-- Fin du bloc pour chaque projet -->
         <div class="p-4 md:w-1/3">
           <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-            <img class="lg:h-48 md:h-36 w-full object-fit object-center" src="/src/assets/img/svg/logo_tune_up.svg"
+            <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="/src/assets/img/jpg/img_tune_up.jpg"
               alt="project" />
             <div class="p-6 bg-bleu-pale">
               <h2 class="text-base font-medium text-indigo-500 mb-1">
