d67667953cc2210066c8cdfc1aafe5ff8b788fb7
Chadrik Bertrand <2037079@cstj.qc.ca>
2023-12-01 12:37:50 -0500

    fix-logo

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -6,9 +6,7 @@
           <h2 class="about inline-block title-font text-6xl font-Kalam w- z-10">
             Mes Compétences
           </h2>
-          <span
-            class="absolute w-full h-1/4 bg-orange-reg bottom-2 rounded-full z-0 opacity-75 skew-y-1"
-          ></span>
+          <span class="absolute w-full h-1/4 bg-orange-reg bottom-2 rounded-full z-0 opacity-75 skew-y-1"></span>
         </div>
         <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
           Les technologies et outils que je maîtrise
@@ -16,10 +14,8 @@
       </div>
       <div class="skills flex flex-wrap gap-2 justify-center">
         <!-- Div à répéter -->
-        <div
-          class="wrapper-skills bg-gris w-1/5 p-3 rounded flex flex-row place-content-evenly"
-        >
-          <img src="png/html-5.png" alt="" class="w-16 h-16" />
+        <div class="wrapper-skills bg-gris w-1/5 p-3 rounded flex flex-row place-content-evenly">
+          <img src="" alt="" class="w-16 h-16" />
           <div class="wrapper-name flex flex-col items-center">
             <h2 class="text-3xl font-bold font-courgette">HTML</h2>
             <p class="annee text-center font-Courgette">
