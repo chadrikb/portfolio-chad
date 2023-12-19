13a988c752e0da087f11e107917bf27c95c09165
Chadrik Bertrand <2037079@cstj.qc.ca>
2023-12-19 14:27:32 -0500

    feature-nav-footer

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,14 +8,14 @@
         <div class="flex items-center">
           <nav class="md:flex items-center justify-end md:flex-1 ">
             <a @click="scrollToSection('accueil')"
-              class="cursor-pointer px-5 py-3 flex items-center text-sm uppercase font-medium leading-snug text-gray-800 hover:text-orange-reg hover:font-bold">Accueil</a>
+              class="cursor-pointer px-5 py-3 flex items-center text-lg uppercase font-medium leading-snug text-gray-800 hover:text-orange-reg hover:font-bold">Accueil</a>
             <a @click="scrollToSection('AboutMe')"
-              class="cursor-pointer px-5 py-3 flex items-center text-sm uppercase font-medium leading-snug text-gray-800 hover:font-bold hover:text-orange-reg">À
+              class="cursor-pointer px-5 py-3 flex items-center text-lg uppercase font-medium leading-snug text-gray-800 hover:font-bold hover:text-orange-reg">À
               Propos</a>
             <a @click="scrollToSection('Skills')"
-              class="cursor-pointer px-5 py-3 flex items-center text-sm uppercase font-medium leading-snug text-gray-800 hover:font-bold hover:text-orange-reg">Compétences</a>
+              class="cursor-pointer px-5 py-3 flex items-center text-lg uppercase font-medium leading-snug text-gray-800 hover:font-bold hover:text-orange-reg">Compétences</a>
             <a @click="scrollToSection('Projects')"
-              class="cursor-pointer px-5 py-3 flex items-center text-sm uppercase font-medium leading-snug text-gray-800 hover:font-bold hover:text-orange-reg">Projets</a>
+              class="cursor-pointer px-5 py-3 flex items-center text-lg uppercase font-medium leading-snug text-gray-800 hover:font-bold hover:text-orange-reg">Projets</a>
           </nav>
         </div>
       </div>
